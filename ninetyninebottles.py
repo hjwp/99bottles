3420ca2b3b352ecf3d04c5d43b9647d833178887
Harry <hjwp2@cantab.net>
2018-02-15 04:17:41 +0000

    First commit, unit tests, shameless-green implementation, no classes

---
+++ ninetyninebottles.py
@@ -0,0 +1,32 @@
+def verse(n):
+    if n == 0:
+        return (
+            "No more bottles of beer on the wall, "
+            "no more bottles of beer.\n"
+            "Go to the store and buy some more, "
+            "99 bottles of beer on the wall.\n"
+        )
+    if n == 1:
+        return (
+            "1 bottle of beer on the wall, "
+            "1 bottle of beer.\n"
+            "Take it down and pass it around, "
+            "no more bottles of beer on the wall.\n"
+        )
+    if n == 2:
+        return (
+            "2 bottles of beer on the wall, "
+            "2 bottles of beer.\n"
+            "Take one down and pass it around, "
+            "1 bottle of beer on the wall.\n"
+        )
+
+    return (
+        f"{n} bottles of beer on the wall, "
+        f"{n} bottles of beer.\n"
+        "Take one down and pass it around, "
+        f"{n-1} bottles of beer on the wall.\n"
+    )
+
+def sing():
+    return '\n'.join(verse(n) for n in range(99, -1, -1))
