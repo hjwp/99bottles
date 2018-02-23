c236ec0de31ad8b713ef601018da0a3587a55cfc
Harry <hjwp2@cantab.net>
2018-02-23 18:40:10 +0200

    n -> number

--- ninetyninebottles.py
+++ ninetyninebottles.py
@@ -1,19 +1,19 @@
-def verse(n):
-    if n == 0:
+def verse(number):
+    if number == 0:
         return (
             "No more bottles of beer on the wall, "
             "no more bottles of beer.\n"
             "Go to the store and buy some more, "
             "99 bottles of beer on the wall.\n"
         )
-    if n == 1:
+    if number == 1:
         return (
             "1 bottle of beer on the wall, "
             "1 bottle of beer.\n"
             "Take it down and pass it around, "
             "no more bottles of beer on the wall.\n"
         )
-    if n == 2:
+    if number == 2:
         return (
             "2 bottles of beer on the wall, "
             "2 bottles of beer.\n"
@@ -22,10 +22,10 @@ def verse(n):
         )
 
     return (
-        f"{n} bottles of beer on the wall, "
-        f"{n} bottles of beer.\n"
+        f"{number} bottles of beer on the wall, "
+        f"{number} bottles of beer.\n"
         "Take one down and pass it around, "
-        f"{n-1} bottles of beer on the wall.\n"
+        f"{number - 1} bottles of beer on the wall.\n"
     )
 
 def sing():
