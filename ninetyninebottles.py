4bdd9ba74ca980195542aa9fdd7b10ba0f8d382c
Harry <hjwp2@cantab.net>
2018-02-23 18:50:23 +0200

    Listing 3.6, replace hard-coded number in 2. also 3.7

--- ninetyninebottles.py
+++ ninetyninebottles.py
@@ -15,10 +15,10 @@ def verse(number):
         )
     if number == 2:
         return (
-            "2 bottles of beer on the wall, "
-            "2 bottles of beer.\n"
+            f"{number} bottles of beer on the wall, "
+            f"{number} bottles of beer.\n"
             "Take one down and pass it around, "
-            "1 bottle of beer on the wall.\n"
+            f"{number - 1} bottle of beer on the wall.\n"
         )
 
     return (
