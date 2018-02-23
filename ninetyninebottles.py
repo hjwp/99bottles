848616bda9dc3f0fbbcab0892d86434b58ad7b59
Harry <hjwp2@cantab.net>
2018-02-23 18:49:10 +0200

    Listing 3.3, 3.4, 3.5 revert last

--- ninetyninebottles.py
+++ ninetyninebottles.py
@@ -20,20 +20,6 @@ def verse(number):
             "Take one down and pass it around, "
             "1 bottle of beer on the wall.\n"
         )
-    if number == 6:
-        return (
-            "1 six-pack of beer on the wall, "
-            "1 six-pack of beer.\n"
-            "Take one down and pass it around, "
-            "5 bottles of beer on the wall.\n"
-        )
-    if number == 7:
-        return (
-            "7 bottles of beer on the wall, "
-            "7 bottles of beer.\n"
-            "Take one down and pass it around, "
-            "1 six-pack of beer on the wall.\n"
-        )
 
     return (
         f"{number} bottles of beer on the wall, "
