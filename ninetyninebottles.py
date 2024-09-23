f3af34a0896bb52b17b90897502c3f080c897448
Harry <hjwp2@cantab.net>
2024-09-24 00:52:45 +0100

    get out one more special case

--- ninetyninebottles.py
+++ ninetyninebottles.py
@@ -2,6 +2,10 @@ def container(n: int) -> str:
     return "bottle" if n == 1 else "bottles"
 
 
+def quantity(n: int) -> str:
+    return "no more" if n == 0 else str(n)
+
+
 def verse(n: int) -> str:
     match n:
         case 0:
@@ -9,15 +13,11 @@ def verse(n: int) -> str:
                 f"No more {container(n)} of beer on the wall, no more {container(n)} of beer.\n"
                 f"Go to the store and buy some more, 99 {container(99)} of beer on the wall.\n"
             )
-        case 1:
-            return (
-                f"1 {container(n)} of beer on the wall, 1 {container(n)} of beer.\n"
-                f"Take it down and pass it around, no more {container(n-1)} of beer on the wall.\n"
-            )
         case _:
+            pronoun = "it" if n == 1 else "one"
             return (
                 f"{n} {container(n)} of beer on the wall, {n} {container(n)} of beer.\n"
-                f"Take one down and pass it around, {n-1} {container(n-1)} of beer on the wall.\n"
+                f"Take {pronoun} down and pass it around, {quantity(n-1)} {container(n-1)} of beer on the wall.\n"
             )
 
 
