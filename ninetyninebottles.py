455f953495b206cdc0533f2b307cf714d8937883
Harry <hjwp2@cantab.net>
2024-09-24 00:49:12 +0100

    refactor out one thing

--- ninetyninebottles.py
+++ ninetyninebottles.py
@@ -1,26 +1,25 @@
-def verse(n):
+def container(n: int) -> str:
+    return "bottle" if n == 1 else "bottles"
+
+
+def verse(n: int) -> str:
     match n:
         case 0:
             return (
-                "No more bottles of beer on the wall, no more bottles of beer.\n"
-                "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+                f"No more {container(n)} of beer on the wall, no more {container(n)} of beer.\n"
+                f"Go to the store and buy some more, 99 {container(99)} of beer on the wall.\n"
             )
         case 1:
             return (
-                "1 bottle of beer on the wall, 1 bottle of beer.\n"
-                "Take it down and pass it around, no more bottles of beer on the wall.\n"
-            )
-        case 2:
-            return (
-                "2 bottles of beer on the wall, 2 bottles of beer.\n"
-                "Take one down and pass it around, 1 bottle of beer on the wall.\n"
+                f"1 {container(n)} of beer on the wall, 1 {container(n)} of beer.\n"
+                f"Take it down and pass it around, no more {container(n-1)} of beer on the wall.\n"
             )
         case _:
             return (
-                f"{n} bottles of beer on the wall, {n} bottles of beer.\n"
-                f"Take one down and pass it around, {n-1} bottles of beer on the wall.\n"
+                f"{n} {container(n)} of beer on the wall, {n} {container(n)} of beer.\n"
+                f"Take one down and pass it around, {n-1} {container(n-1)} of beer on the wall.\n"
             )
 
 
-def sing():
+def sing() -> str:
     return "\n".join(verse(n) for n in range(99, -1, -1))
