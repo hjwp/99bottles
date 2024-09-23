638b357afe2303aa6fc59a8952bda87fd76497e1
Harry <hjwp2@cantab.net>
2024-09-24 00:45:13 +0100

    back to shameless green

--- ninetyninebottles.py
+++ ninetyninebottles.py
@@ -1,69 +1,26 @@
-class Bottles:
-    """
-    Represents a number of bottles (maybe zero),
-    how to talk about them, and how to get the next number of bottles
-    """
-    def __init__(self, number):
-        self.number = number
-
-    def __str__(self):
-        return f'{self.number} bottles'
-
-    @property
-    def action(self):
-        return f'take {self.pronoun} down and pass it around'
-
-    @property
-    def pronoun(self):
-        return 'one'
-
-    @property
-    def successor(self):
-        return Bottles.for_number(self.number - 1)
-
-    @staticmethod
-    def for_number(number):
-        if number == 0:
-            return NoBottles()
-        if number == 1:
-            return OneBottle()
-        return Bottles(number)
-
-
-class NoBottles(Bottles):
-    def __init__(self):
-        self.number = 0
-
-    def __str__(self):
-        return 'no more bottles'
-
-    @property
-    def action(self):
-        return 'go to the store and buy some more'
-
-    @property
-    def successor(self):
-        return Bottles.for_number(99)
-
-
-class OneBottle(Bottles):
-    def __init__(self):
-        self.number = 1
-
-    def __str__(self):
-        return '1 bottle'
-
-    @property
-    def pronoun(self):
-        return 'it'
-
-
 def verse(n):
-    bottles = Bottles.for_number(n)
-    return (
-        f"{bottles} of beer on the wall, {bottles} of beer.\n".capitalize() +
-        f"{bottles.action}, {bottles.successor} of beer on the wall.\n".capitalize()
-    )
+    match n:
+        case 0:
+            return (
+                "No more bottles of beer on the wall, no more bottles of beer.\n"
+                "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+            )
+        case 1:
+            return (
+                "1 bottle of beer on the wall, 1 bottle of beer.\n"
+                "Take it down and pass it around, no more bottles of beer on the wall.\n"
+            )
+        case 2:
+            return (
+                "2 bottles of beer on the wall, 2 bottles of beer.\n"
+                "Take one down and pass it around, 1 bottle of beer on the wall.\n"
+            )
+        case _:
+            return (
+                f"{n} bottles of beer on the wall, {n} bottles of beer.\n"
+                f"Take one down and pass it around, {n-1} bottles of beer on the wall.\n"
+            )
+
 
 def sing():
-    return '\n'.join(verse(n) for n in range(99, -1, -1))
+    return "\n".join(verse(n) for n in range(99, -1, -1))
