7314cfa702e28785db8d5d5c6c2bb1beba2d0286
Harry <hjwp2@cantab.net>
2025-06-16 12:13:23 +0100

    incomprehensibly concise

--- ninetyninebottles.py
+++ ninetyninebottles.py
@@ -1,30 +1,14 @@
-from textwrap import dedent
-
-
-def _describe_bottles(n: int) -> str:
-    if n == 0:
-        return "no more bottles"
-    elif n == 1:
-        return "1 bottle"
-    else:
-        return f"{n} bottles"
-
-
 def verse(n: int) -> str:
-    these_bottles = _describe_bottles(n)
-    shelf = (
-        f"{these_bottles} of beer on the wall, {these_bottles} of beer.".capitalize()
+    return (
+        f"{'No more' if n == 0 else n} bottle{'s' if n != 1 else ''} of beer on the wall, "
+        + f"{'no more' if n == 0 else n} bottle{'s' if n != 1 else ''} of beer.\n"
+        + (
+            "Go to the store and buy some more, "
+            if n == 0
+            else f"Take {'it' if n == 1 else 'one'} down and pass it around, "
+        )
+        + f"{'no more' if n == 1 else 99 if n == 0 else n - 1} bottle{'s' if n != 2 else ''} of beer on the wall.\n"
     )
-
-    if n == 0:
-        action = "Go to the store and buy some more"
-        next_bottles = _describe_bottles(99)
-    else:
-        next_bottles = _describe_bottles(n - 1)
-        this_bottle = "it" if n == 1 else "one"
-        action = f"Take {this_bottle} down and pass it around"
-
-    return f"{shelf}\n{action}, {next_bottles} of beer on the wall.\n"
 
 
 def sing() -> str:
