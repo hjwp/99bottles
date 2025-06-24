f655c08e429e39ae0365d1cf79d584cf7af0ca2e
Harry <hjwp2@cantab.net>
2025-06-24 17:41:50 +0100

    verses now works not that i see much point in it also fix some bugs

--- ninetyninebottles.py
+++ ninetyninebottles.py
@@ -3,14 +3,14 @@ def verse(number: int) -> str:
         case 0:
             return (
                 "No more bottles of beer on the wall, "
-                "No more bottles of beer.\n"
+                "no more bottles of beer.\n"
                 "Go to the store and buy some more, "
                 "99 bottles of beer on the wall.\n"
             )
         case 1:
             return (
-                "1 bottles of beer on the wall, "
-                "1 bottles of beer.\n"
+                "1 bottle of beer on the wall, "
+                "1 bottle of beer.\n"
                 "Take it down and pass it around, "
                 "no more bottles of beer on the wall.\n"
             )
@@ -28,3 +28,6 @@ def verse(number: int) -> str:
                 "Take one down and pass it around, "
                 f"{number - 1} bottles of beer on the wall.\n"
             )
+
+def verses(start: int, end: int) -> str:
+    return "\n".join(verse(n) for n in range(start, end - 1, -1))
--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-from ninetyninebottles import verse
+from ninetyninebottles import verse, verses
 
 
 def test_the_first_verse() -> None:
@@ -33,18 +33,54 @@ def test_verse_2() -> None:
 
 def test_verse_1() -> None:
     expected = (
-        "1 bottles of beer on the wall, "
-        "1 bottles of beer.\n"
+        "1 bottle of beer on the wall, "
+        "1 bottle of beer.\n"
         "Take it down and pass it around, "
         "no more bottles of beer on the wall.\n"
     )
     assert verse(1) == expected
 
+
 def test_verse_0() -> None:
     expected = (
         "No more bottles of beer on the wall, "
-        "No more bottles of beer.\n"
+        "no more bottles of beer.\n"
         "Go to the store and buy some more, "
         "99 bottles of beer on the wall.\n"
     )
     assert verse(0) == expected
+
+
+def test_a_couple_verses() -> None:
+    expected = (
+        "99 bottles of beer on the wall, "
+        "99 bottles of beer.\n"
+        "Take one down and pass it around, "
+        "98 bottles of beer on the wall.\n"
+        "\n"
+        "98 bottles of beer on the wall, "
+        "98 bottles of beer.\n"
+        "Take one down and pass it around, "
+        "97 bottles of beer on the wall.\n"
+    )
+    assert verses(99, 98) == expected
+
+
+def test_a_few_verses() -> None:
+    expected = (
+        "2 bottles of beer on the wall, " +
+        "2 bottles of beer.\n" +
+        "Take one down and pass it around, " +
+        "1 bottle of beer on the wall.\n" +
+        "\n" +
+        "1 bottle of beer on the wall, " +
+        "1 bottle of beer.\n" +
+        "Take it down and pass it around, " +
+         "no more bottles of beer on the wall.\n" +
+         "\n" +
+         "No more bottles of beer on the wall, " +
+         "no more bottles of beer.\n" +
+         "Go to the store and buy some more, " +
+         "99 bottles of beer on the wall.\n"
+    )
+    assert verses(2, 0) == expected
