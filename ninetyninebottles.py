189071d47fecbadac525841933b52aeb0fe36da6
Harry <hjwp2@cantab.net>
2025-07-07 18:03:41 +0100

    quantity and pronoun functions

--- ninetyninebottles.py
+++ ninetyninebottles.py
@@ -2,30 +2,28 @@ def verse(number: int) -> str:
     match number:
         case 0:
             return (
-                f"No more {_container(number)} of beer on the wall, "
-                f"no more {_container(number)} of beer.\n"
+                f"{_quantity(number)} {_container(number)} of beer on the wall, ".capitalize() +
+                f"{_quantity(number)} {_container(number)} of beer.\n"
                 f"Go to the store and buy some more, "
-                f"99 {_container(number - 1)} of beer on the wall.\n"
-            )
-        case 1:
-            return (
-                f"{number} {_container(number)} of beer on the wall, "
-                f"{number} {_container(number)} of beer.\n"
-                f"Take it down and pass it around, "
-                f"no more {_container(number - 1)} of beer on the wall.\n"
+                f"99 {_container(number - 1)} of beer on the wall.\n"  # TODO
             )
         case _:
             return (
-                f"{number} {_container(number)} of beer on the wall, "
+                f"{_quantity(number)} {_container(number)} of beer on the wall, ".capitalize() +
                 f"{number} {_container(number)} of beer.\n"
-                "Take one down and pass it around, "
-                f"{number - 1} {_container(number - 1)} of beer on the wall.\n"
+                f"Take {_pronoun(number)} down and pass it around, "
+                f"{_quantity(number - 1)} {_container(number - 1)} of beer on the wall.\n"
             )
 
 
 def _container(number: int) -> str:
     return "bottle" if number == 1 else "bottles"
 
+def _pronoun(number: int) -> str:
+    return "it" if number == 1 else "one"
+
+def _quantity(number: int) -> str:
+    return "no more" if number == 0 else str(number)
 
 def verses(start: int, end: int) -> str:
     return "\n".join(verse(n) for n in range(start, end - 1, -1))
--- tests.py
+++ tests.py
@@ -70,20 +70,20 @@ def test_a_couple_verses() -> None:
 
 def test_a_few_verses() -> None:
     expected = (
-        "2 bottles of beer on the wall, " +
-        "2 bottles of beer.\n" +
-        "Take one down and pass it around, " +
-        "1 bottle of beer on the wall.\n" +
-        "\n" +
-        "1 bottle of beer on the wall, " +
-        "1 bottle of beer.\n" +
-        "Take it down and pass it around, " +
-         "no more bottles of beer on the wall.\n" +
-         "\n" +
-         "No more bottles of beer on the wall, " +
-         "no more bottles of beer.\n" +
-         "Go to the store and buy some more, " +
-         "99 bottles of beer on the wall.\n"
+        "2 bottles of beer on the wall, "
+        + "2 bottles of beer.\n"
+        + "Take one down and pass it around, "
+        + "1 bottle of beer on the wall.\n"
+        + "\n"
+        + "1 bottle of beer on the wall, "
+        + "1 bottle of beer.\n"
+        + "Take it down and pass it around, "
+        + "no more bottles of beer on the wall.\n"
+        + "\n"
+        + "No more bottles of beer on the wall, "
+        + "no more bottles of beer.\n"
+        + "Go to the store and buy some more, "
+        + "99 bottles of beer on the wall.\n"
     )
     assert verses(2, 0) == expected
 
