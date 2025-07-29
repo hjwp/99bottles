bdc0b5bdd02b4200f96bb826a2730ed700d39267
Harry <hjwp2@cantab.net>
2025-07-29 15:49:30 +0100

    extract BottleNumber class

--- ninetyninebottles.py
+++ ninetyninebottles.py
@@ -1,40 +1,45 @@
+from dataclasses import dataclass
+
+
 def verse(number: int) -> str:
+    bn = BottleNumber(number)
+    next_bn = BottleNumber(bn.next())
     return (
-        f"{_quantity(number).capitalize()} {_container(number)} of beer on the wall, "
-        f"{_quantity(number)} {_container(number)} of beer.\n"
-        f"{_action(number).capitalize()}, "
-        f"{_quantity(_next(number))} {_container(_next(number))} of beer on the wall.\n"
+        f"{bn.quantity().capitalize()} {bn.container()} of beer on the wall, "
+        f"{bn.quantity()} {bn.container()} of beer.\n"
+        f"{bn.action().capitalize()}, "
+        f"{next_bn.quantity()} {next_bn.container()} of beer on the wall.\n"
     )
 
 
-def _container(number: int) -> str:
-    if number == 1:
-        return "bottle"
-    return "bottles"
+@dataclass
+class BottleNumber:
+    num_bottles: int
 
+    def container(self) -> str:
+        if self.num_bottles == 1:
+            return "bottle"
+        return "bottles"
 
-def _pronoun(number: int) -> str:
-    if number == 1:
-        return "it"
-    return "one"
+    def _pronoun(self) -> str:
+        if self.num_bottles == 1:
+            return "it"
+        return "one"
 
+    def quantity(self) -> str:
+        if self.num_bottles == 0:
+            return "no more"
+        return str(self.num_bottles)
 
-def _quantity(number: int) -> str:
-    if number == 0:
-        return "no more"
-    return str(number)
+    def action(self) -> str:
+        if self.num_bottles == 0:
+            return "go to the store and buy some more"
+        return f"take {self._pronoun()} down and pass it around"
 
-
-def _action(number: int) -> str:
-    if number == 0:
-        return "go to the store and buy some more"
-    return f"take {_pronoun(number)} down and pass it around"
-
-
-def _next(number: int) -> int:
-    if number == 0:
-        return 99
-    return number - 1
+    def next(self) -> int:
+        if self.num_bottles == 0:
+            return 99
+        return self.num_bottles - 1
 
 
 def verses(start: int, end: int) -> str:
