02fdc64d2a3773feae9fa45a6614de27f75ac791
Harry <hjwp2@cantab.net>
2025-08-06 02:47:05 +0100

    add onebottole

--- ninetyninebottles.py
+++ ninetyninebottles.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 
 def verse(number: int) -> str:
     this_bottles = Bottles.build(number)
-    next_bottles = Bottles(
+    next_bottles = Bottles.build(
         this_bottles.next_number  # TODO: this feels awkward
     )
     line1 = f"{this_bottles} of beer on the wall, {this_bottles} of beer"
@@ -13,56 +13,49 @@ def verse(number: int) -> str:
     return f"{line1.capitalize()}.\n{line2.capitalize()}.\n"
 
 
+@dataclass
 class Bottles:
-    def __init__(self, num_bottles: int) -> None:
-        self._num_bottles = num_bottles
+    _num_bottles: int
+    _pronoun: str = "one"
+    container: str = "bottles"
 
     @classmethod
     def build(cls, num_bottles: int) -> Bottles:
         if num_bottles == 0:
-            return NoBottles(0)
+            return NoBottles()
+        if num_bottles == 1:
+            return OneBottle()
         return cls(num_bottles)
 
     def __str__(self) -> str:
         return f"{self.quantity} {self.container}"
 
     @property
-    def container(self) -> str:
-        if self._num_bottles == 1:
-            return "bottle"
-        return "bottles"
-
-    @property
-    def _pronoun(self) -> str:
-        if self._num_bottles == 1:
-            return "it"
-        return "one"
-
-    @property
     def quantity(self) -> str:
-        if self._num_bottles == 0:
-            return "no more"
         return str(self._num_bottles)
 
     @property
     def action(self) -> str:
-        if self._num_bottles == 0:
-            return "go to the store and buy some more"
         return f"take {self._pronoun} down and pass it around"
 
     @property
     def next_number(self) -> int:
-        if self._num_bottles == 0:
-            return 99
         return self._num_bottles - 1
 
 
 @dataclass
 class NoBottles(Bottles):
-    _num_bottles: int
+    _num_bottles: int = 0
     quantity: str = "no more"
     action: str = "go to the store and buy some more"
     next_number: int = 99
+
+
+@dataclass
+class OneBottle(Bottles):
+    _num_bottles: int = 1
+    _pronoun: str = "it"
+    container: str = "bottle"
 
 
 def verses(start: int, end: int) -> str:
